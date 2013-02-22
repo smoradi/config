c15fffcf2468f244eded5395c5c04eb6e1913d02
FreeBSD-9.0 Developer <developer@freebsd.90>
2013-02-22 10:37:58 +0000

    Some minor configuration changes, plus freeBsd90InstalledPackages.txt

--- oscripts/root_login.sh
+++ oscripts/root_login.sh
@@ -1,13 +1,13 @@
 echo "GoodMorning $0"
 #kill `cat /var/run/syslog.pid`
 #echo "syslogd -ss"
-syslogd -ss
+#syslogd -ss
 
 set JAVA_HOME=/usr/local/diablo-jdk1.6.0
 
 echo "vidcontrol -h 7000"
 vidcontrol -h 7000
-echo "sockstat -l"
-sockstat -l
+echo "sockstat -4l"
+sockstat -4l
 echo "w"
 w
