afe8ad3d00e38bcf1d35f1167b8ba1b0ace7b552
FreeBSD-9.0 Developer <developer@freebsd.90>
2013-08-24 15:44:41 +0000

    minor useful config commands, plus hosts.allow.txt config shows filter by IP range. logout_r.sh is useful script to clean home directory with reverse pattern. For example it deletes everything except those included in script. Note: use logout_r.sh carefully!

---
+++ oscripts/logout_r.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+echo "Home cleaning ..."
+cd ~
+
+junk=`ls -a |grep -E -v '(\.|\.\.|logout|shrc|cshrc|vim|vimrc|login|login_conf|profile|rhosts|mailrc|mail_aliases|kde|gem|m2|mozilla|opera)$|^(Desktop|opt|kernels)'`
+
+for fname in $junk; do
+rm -rfv "$fname"
+done
