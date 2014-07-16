7268be6fe8e246a3f503c45b54618751b7e49888
FreeBSD-9.0 Developer <developer@freebsd.90>
2014-07-16 13:29:34 +0000

    xwd-1.0.4 installed as of this revision.

--- oscripts/logout_r.sh
+++ oscripts/logout_r.sh
@@ -3,7 +3,7 @@
 echo "Home cleaning ..."
 cd ~
 
-junk=`ls -a |grep -E -v '(\.|\.\.|logout|shrc|cshrc|vim|vimrc|mavenrc|login|login_conf|profile|rhosts|mailrc|mail_aliases|kde|gem|m2|mozilla|opera)$|^(Desktop|opt|kernels)'`
+junk=`ls -a |grep -E -v '(\.|\.\.|logout|shrc|cshrc|vim|vimrc|mavenrc|login|login_conf|profile|rhosts|mailrc|mail_aliases|ssh|kde|gem|m2|mozilla|opera)$|^(Desktop|opt|kernels)'`
 
 for fname in $junk; do
 rm -rfv "$fname"
