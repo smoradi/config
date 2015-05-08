a42c9ecb17a0a2fb245642e43e98e652bb356c93
Android <andro@h.(none)>
2015-05-08 11:04:46 +0000

    wittertime: God(programmer) + you-see-what-you-want-to-see = smile (-:-)

--- oscripts/logout_r.sh
+++ oscripts/logout_r.sh
@@ -3,7 +3,7 @@
 echo "Home cleaning ..."
 cd ~
 
-junk=`ls -a |grep -E -v '(\.|\.\.|logout|shrc|cshrc|vim|vimrc|rvm|mavenrc|login|login_conf|profile|rhosts|mailrc|mail_aliases|ssh|kde|gem|m2|dropbox-api-config|android|mozilla|opera)$|^(Desktop|80.txt|android-sdk-linux|opt|kernels)'`
+junk=`ls -a |grep -E -v '(\.|\.\.|logout|shrc|cshrc|vim|vimrc|rvm|npm|mavenrc|login|login_conf|profile|rhosts|mailrc|mail_aliases|ssh|kde|gem|m2|dropbox-api-config|android|gradle|sqldeveloper|mozilla|opera)$|^(Desktop|80.txt|android-sdk-linux|opt|kernels)'`
 
 for fname in $junk; do
 rm -rfv "$fname"
